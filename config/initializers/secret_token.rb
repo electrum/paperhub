06c7617d66015ce9c0c05901572b8ac6fe078f26
David Phillips <david@acz.org>
2011-04-01 23:05:08 -0700

    Fresh app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paperhub::Application.config.secret_token = 'e1df140a8dd2366857c1364a782db4f276d3383c44ee96eb3d5e7808b7b83a71eb1877b08d561079d19eafacdd040a7beefa137ffb2e3b627215e55f53bde0d3'
