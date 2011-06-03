9fde449711f3a90ed6a216a73b1a1baf49664817
David Phillips <david@acz.org>
2011-06-03 16:08:17 -0700

    Add Gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def gravatar_url(email, size = 80)
+    hash = Digest::MD5::hexdigest(email).downcase
+    "http://www.gravatar.com/avatar/#{hash}?s=#{size.to_i}&d=mm"
+  end
 end
